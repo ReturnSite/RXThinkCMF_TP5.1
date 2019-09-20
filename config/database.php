705a3a30fa000fcc2915e129e6603cd1da4753d7
zongjl <zongjinlong@yunhengzhizao.com>
2019-09-20 08:59:04 +0800

    '更新'

--- config/database.php
+++ config/database.php
@@ -1,37 +1,46 @@
 <?php
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// | RXThinkCMF框架 [ RXThinkCMF ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2017 http://thinkphp.cn All rights reserved.
+// | 版权所有 2017~2019 南京RXThink工作室
 // +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// | 官方网站: http://www.rxthink.cn
 // +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
+// | Author: 牧羊人 <rxthink.cn@gmail.com>
 // +----------------------------------------------------------------------
+
+use think\facade\Config;
+
+// 获取自定义配置文件
+$config = Config::get('app.config');
+$db_config = $config['db_config'];
+$itemArr = explode('://', $db_config);
+$db_type = $itemArr[0];
+list($db_user, $db_pwd, $db_host, $db_port, $db_name) = preg_split("/[:@\/]/", $itemArr[1]);
 
 return [
     // 数据库类型
-    'type' => 'mysql',
+    'type' => $db_type,//Env::get('database.type', 'mysql'),
     // 服务器地址
-    'hostname' => '127.0.0.1',
+    'hostname' => $db_host,//Env::get('database.hostname', '127.0.0.1'),
     // 数据库名
-    'database' => 'rxthink.v5.1_2',
+    'database' => $db_name,//Env::get('database.database', 'rxthink'),
     // 用户名
-    'username' => 'root',
+    'username' => $db_user,//Env::get('database.username', 'root'),
     // 密码
-    'password' => '',
+    'password' => $db_pwd,//Env::get('database.password', ''),
     // 端口
-    'hostport' => '3306',
+    'hostport' => $db_port,//Env::get('database.hostport', ''),
     // 连接dsn
     'dsn' => '',
     // 数据库连接参数
     'params' => [],
-    // 数据库编码默认采用utf8mb4
-    'charset' => 'utf8mb4',
+    // 数据库编码默认采用utf8
+    'charset' => $config['db_charset'],//Env::get('database.charset', 'utf8'),
     // 数据库表前缀
-    'prefix' => 'think_',
+    'prefix' => $config['db_prefix'],//Env::get('database.prefix', ''),
     // 数据库调试模式
-    'debug' => true,
+    'debug' => true,//Env::get('database.debug', true),
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
     'deploy' => 0,
     // 数据库读写是否分离 主从式有效
